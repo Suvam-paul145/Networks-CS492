d8f3018f97d824c3f06f2333163879afc7e334b8
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-26 13:08:35 +0530

    Refactor bit stuffing and destuffing functions to use dynamic memory and C-style arrays

--- Module 2/stuffing-destuffing-of-numericals.c
+++ Module 2/stuffing-destuffing-of-numericals.c
@@ -1,70 +1,77 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
-vector<int> bitStuffing(vector<int> data) {
-    vector<int> stuffedData;
-    int count = 0;
+// Function to perform bit stuffing
+int* bitStuffing(int* data, int size, int* stuffedSize) {
+    int* stuffedData = (int*)malloc(size * 2 * sizeof(int));
+    int count = 0, j = 0;
 
-    for (int bit : data) {
-        if (bit == 1) {
+    for (int i = 0; i < size; i++) {
+        if (data[i] == 1) {
             count++;
         } else {
             count = 0;
         }
-        stuffedData.push_back(bit);
+        stuffedData[j++] = data[i];
         if (count == 5) {
-            stuffedData.push_back(0);
+            stuffedData[j++] = 0;
             count = 0;
         }
     }
+    *stuffedSize = j;
     return stuffedData;
 }
 
+// Function to perform bit destuffing
+int* bitDestuffing(int* data, int size, int* destuffedSize) {
+    int* destuffedData = (int*)malloc(size * sizeof(int));
+    int count = 0, j = 0;
 
-4
-vector<int> bitDestuffing(vector<int> data) {
-    vector<int> destuffedData;
-    int count = 0;
-
-    for (int bit : data) {
-        if (bit == 1) {
+    for (int i = 0; i < size; i++) {
+        if (data[i] == 1) {
             count++;
         } else {
             count = 0;
         }
-        destuffedData.push_back(bit);
+        destuffedData[j++] = data[i];
         if (count == 5) {
-            if (data.size() > destuffedData.size() && data[destuffedData.size()] == 0) {
-                destuffedData.pop_back();
+            if (i + 1 < size && data[i + 1] == 0) {
+                i++;
                 count = 0;
             }
         }
     }
+    *destuffedSize = j;
     return destuffedData;
 }
 
 int main() {
-    vector<int> data = {1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0};
-    cout << "Original Data: ";
-    for (int bit : data) {
-        cout << bit;
-    }
-    cout << endl;
+    int data[] = {1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0};
+    int size = sizeof(data) / sizeof(data[0]);
+    int stuffedSize, destuffedSize;
 
-    vector<int> stuffedData = bitStuffing(data);
-    cout << "Stuffed Data: ";
-    for (int bit : stuffedData) {
-        cout << bit;
+    printf("Original Data: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d", data[i]);
     }
-    cout << endl;
+    printf("\n");
 
-    vector<int> destuffedData = bitDestuffing(stuffedData);
-    cout << "Destuffed Data: ";
-    for (int bit : destuffedData) {
-        cout << bit;
+    int* stuffedData = bitStuffing(data, size, &stuffedSize);
+    printf("Stuffed Data: ");
+    for (int i = 0; i < stuffedSize; i++) {
+        printf("%d", stuffedData[i]);
     }
-    cout << endl;
+    printf("\n");
+
+    int* destuffedData = bitDestuffing(stuffedData, stuffedSize, &destuffedSize);
+    printf("Destuffed Data: ");
+    for (int i = 0; i < destuffedSize; i++) {
+        printf("%d", destuffedData[i]);
+    }
+    printf("\n");
+
+    free(stuffedData);
+    free(destuffedData);
 
     return 0;
 }
