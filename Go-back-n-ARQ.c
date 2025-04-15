51ba29ca1db996a7df28a948e69958af796b2d01
Suvam-paul145 <suvampersonal555@gmail.com>
2025-04-16 00:09:17 +0530

    Refactor Go-Back-N and Stop-and-Wait ARQ implementations for improved code readability

--- Go-back-n-ARQ.c
+++ Go-back-n-ARQ.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int totalFrames, windowSize, i, ack;
 
     printf("Enter total number of frames: ");
@@ -9,19 +10,24 @@ int main() {
     scanf("%d", &windowSize);
 
     int sent = 0;
-    while (sent < totalFrames) {
+    while (sent < totalFrames)
+    {
         printf("\nSending window of frames: ");
-        for (i = 0; i < windowSize && (sent + i) < totalFrames; i++) {
+        for (i = 0; i < windowSize && (sent + i) < totalFrames; i++)
+        {
             printf("%d ", sent + i);
         }
 
         printf("\nEnter index of lost frame in this window (-1 if none): ");
         scanf("%d", &ack);
 
-        if (ack == -1) {
+        if (ack == -1)
+        {
             printf("All ACKs received.\n");
             sent += windowSize;
-        } else {
+        }
+        else
+        {
             printf("ACK not received for frame %d. Go-Back-N triggered.\n", ack);
             sent = ack; // go back to the lost frame
         }
--- stop-and-wait-ARQ.c
+++ stop-and-wait-ARQ.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int frames, i, data, ack;
-    
+
     printf("Enter number of frames: ");
     scanf("%d", &frames);
-    
-    for (i = 0; i < frames; i++) {
+
+    for (i = 0; i < frames; i++)
+    {
         printf("\nEnter data for frame %d: ", i);
         scanf("%d", &data);
-        
-        do {
+
+        do 
+        {
             printf("Sending frame %d (Data: %d)\n", i, data);
             printf("ACK received? (1:Yes/0:No): ");
             scanf("%d", &ack);
-            
-            if (ack) {
+
+            if (ack)
+            {
                 printf("Frame %d transmitted successfully\n", i);
                 break;
             }
             printf("Timeout! Resending frame %d\n", i);
         } while (!ack);
     }
-    
+
     printf("\nAll frames sent successfully!\n");
     return 0;
 }
